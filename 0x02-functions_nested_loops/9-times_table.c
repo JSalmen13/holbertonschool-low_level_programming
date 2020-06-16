473815b85a411d43519e1a0f86dd74ca40861392
JSalmen13 <Jarraya.salmen13@gmail.com>
2020-06-16 14:57:36 +0100

    yey

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+
+/**
+ * times_table - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+void times_table(void)
+{
+	int i, u, y;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (u = 0; u < 10; u++)
+		{
+		y = u * i;
+			if (y < 10)
+				{
+					_putchar(y + '0');
+				}
+			else
+				{
+				_putchar(y / 10 + '0');
+				_putchar(y % 10 + '0');
+				}
+			if (u != 9)
+				{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				}
+		}
+	_putchar('\n');
+	}
+}
