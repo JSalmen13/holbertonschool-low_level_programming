234655feaed0a1eeb3dff49e6c33ec616c9acc56
JSalmen13 <Jarraya.salmen13@gmail.com>
2020-06-16 09:21:20 +0100

    fix the betty style

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,19 +1,20 @@
 #include "holberton.h"
 /**
-*main- display a name
-*calling _putchar
+* main- display a name
+*
+* Return: 0
 **/
 int main(void)
 {
-_putchar('H');
-_putchar('o');
-_putchar('l');
-_putchar('b');
-_putchar('e');
-_putchar('r');
-_putchar('t');
-_putchar('o');
-_putchar('n');
-_putchar('\n');
-return(0);
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+	return (0);
 }
