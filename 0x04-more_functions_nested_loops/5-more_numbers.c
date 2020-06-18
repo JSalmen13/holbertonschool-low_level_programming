463d156f3098c90d93ab5f98c188f7432c3c5357
jsalmen13 <jarraya.salmen13@gmail.com>
2020-06-18 12:54:52 +0100

    correction

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,13 +8,15 @@ void more_numbers(void)
 {
 	int i, j;
 
-	for (j = 0; j < 10; j++)
+	for (j = 0; j < 11; j++)
 	{
 		for (i = 0; i <= 14; i++)
 		{
-			_putchar(i / 10 + '0');
+			if (i >= 10)
+			{
+				_putchar(i / 10 + '0');
+			}
 			_putchar(i % 10 + '0');
-			
 		}
 		_putchar('\n');
 	}
