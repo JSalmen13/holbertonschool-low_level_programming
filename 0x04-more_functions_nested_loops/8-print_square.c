6b4c23922706c45e8313a5ccf6b711a3a793291a
jsalmen13 <jarraya.salmen13@gmail.com>
2020-06-18 19:11:13 +0100

    betty style can be harsh sometimes

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,14 +1,14 @@
 #include "holberton.h"
 /**
 * print_square - square #
-* @n: var
+* @size: square size
 * Return: 0
 **/
 void print_square(int size)
 {
 	int i, j;
 
-	if(size > 0)
+	if (size > 0)
 	{
 		for (i = 0; i < size; i++)
 		{
@@ -18,10 +18,9 @@ void print_square(int size)
 			}
 			_putchar('\n');
 		}
-	}	
+	}
 	else
 	{
 		_putchar('\n');
 	}
-	
 }
