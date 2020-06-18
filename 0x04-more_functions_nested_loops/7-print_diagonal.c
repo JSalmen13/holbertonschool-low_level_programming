f9869a4017117ce92f04bcad58c9f889cebb4274
jsalmen13 <jarraya.salmen13@gmail.com>
2020-06-18 18:01:53 +0100

    delayed push

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+* print_diagonal - diagonal printing
+* @n: var
+* Return: 0
+**/
+void print_diagonal(int n)
+{
+	int i, j = 0;
+
+	for (i = 0; i <= n; i++)
+	{
+		for(j = 0; j <= i; j++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+	
+}
