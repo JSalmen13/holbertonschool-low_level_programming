acc3029bb31634a375ee4f4ce8b7718e9ed9d102
jsalmen13 <jarraya.salmen13@gmail.com>
2020-06-18 19:40:37 +0100

    fuck yea i passed

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,7 +10,7 @@ int main(void)
 
 	for (n = 1; n <= 100; n++)
 	{
-		if ((n % 3 == 0) && (n % 5 == 0))
+		if (n % 3 == 0 && n % 5 == 0)
 		{
 			printf("FizzBuzz");
 		}
@@ -24,7 +24,7 @@ int main(void)
 		}
 		else
 		{
-			printf("%d ", n);
+			printf("%d", n);
 		}
 		if (n != 100)
 		{
