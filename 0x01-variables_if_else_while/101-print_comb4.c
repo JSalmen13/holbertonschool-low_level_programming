cf241443a219d3d38c4737772e3e7815d7237930
JSalmen13 <Jarraya.salmen13@gmail.com>
2020-06-19 09:39:11 +0100

    whyyyy

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,20 +11,20 @@ int main(void)
 	int c;
 
 	for (a = 0; a <= 9; a++)
-		for (b = 0; b <= 9; b++)
-			for (c = 0; c <= 9; c++)
-				if (a < b)
-					if (b < c)
-					{
-						putchar(a + '0');
-						putchar(b + '0');
-						putchar(c + '0');
-						if (a != 7 || b != 8 || c != 9)
-						{
-							putchar(',');
-							putchar(' ');
-						}
-					}
+	for (b = 0; b <= 9; b++)
+	for (c = 0; c <= 9; c++)
+	if (a < b)
+	if (b < c)
+		{
+			putchar(a + '0');
+			putchar(b + '0');
+			putchar(c + '0');
+			if (a != 7 || b != 8 || c != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
 	putchar('\n');
 	return (0);
 }
