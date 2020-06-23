fa8322decf72d521509105779462352ab15f3ce3
JSalmen13 <jarraya.salmen13@gmail.com>
2020-06-23 11:42:21 +0100

    aze

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * print_rev - print in reverse
+ * @s: character
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+	char *p = s;
+
+	while (*s != '\0')
+	{
+		s++;
+		i++;
+	}
+	while (i--)
+	{
+		_putchar (p[i]);		
+	}
+	_putchar ('\n');
+}
