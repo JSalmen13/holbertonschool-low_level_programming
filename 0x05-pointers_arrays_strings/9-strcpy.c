d919b69c3b99c32f9bfafc995f25fd88c67d4cdb
JSalmen13 <64764926+JSalmen13@users.noreply.github.com>
2020-06-23 23:55:30 +0100

    aze

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,11 +8,19 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int i;
+	int a, length;
 
-	for (i = 0 ; src[i] != '\0' ; i++)
+	length = 0;
+	while (src[length] != '\0')
 	{
-		dest[i] = src[i];
+	length++;
 	}
-	return (dest);
+
+
+	for (a = 0; a <= length; a++)
+	{
+		dest[a] = src[a];
+	}
+
+return (dest);
 }
