b7618092648488af4e372e7cc5a84d80081660b1
JSalmen13 <jarraya.salmen13@gmail.com>
2020-06-29 14:58:51 +0100

    add

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * _strchr - search
+ * @s: char
+ * @c: char 2
+ * Return: *s
+ */
+char *_strchr(char *s, char c)
+{
+	char *p = s;
+	
+	while (*p != '\0')
+	{
+		if (*p == c)
+		return (p);
+		p++;
+	}
+	return (0);
+}
