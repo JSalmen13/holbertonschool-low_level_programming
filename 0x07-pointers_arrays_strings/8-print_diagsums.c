baf80882ae58c1733713c67ec3b1c511c8df1980
JSalmen13 <jarraya.salmen13@gmail.com>
2020-06-30 23:38:24 +0100

    finally

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * 
+ * 
+ * 
+ * 
+ */
+void print_diagsums(int *a, int size)
+{
+	int i = 0;
+	int sum1 = 0;
+	int sum2 = 0;
+
+	for (; i < size; i ++)
+	{
+		sum1 = sum1 + *(a + i * size + i);
+		sum2 = sum2 + *(a + i * size + size - 1 - i);
+	}
+	printf("%i, %i\n", sum1, sum2);
+}
