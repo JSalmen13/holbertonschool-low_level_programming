e1f038295484381937e0c1174f5fe8a5be54178a
JSalmen13 <jarraya.salmen13@gmail.com>
2020-06-30 23:40:36 +0100

    fix

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,10 +1,9 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
- * 
- * 
- * 
- * 
+ * print_diagsums - prints diagonal sum
+ * @a: arr
+ * @size: int size of 2D arr
  */
 void print_diagsums(int *a, int size)
 {
@@ -12,7 +11,7 @@ void print_diagsums(int *a, int size)
 	int sum1 = 0;
 	int sum2 = 0;
 
-	for (; i < size; i ++)
+	for (; i < size; i++)
 	{
 		sum1 = sum1 + *(a + i * size + i);
 		sum2 = sum2 + *(a + i * size + size - 1 - i);
