bf1dea4f2af6c2a89e0cf94c81bbd8009426416c
JSalmen13 <jarraya.salmen13@gmail.com>
2020-06-30 23:52:55 +0100

    add

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+* set_string - azeazeazeaze
+* @s: azeazeazeaze
+* @to: azeazeazeaze
+* Return: 0
+*/
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
