ef42a3f80d83595f5994462b5857a2bf72955d91
JSalmen13 <jarraya.salmen13@gmail.com>
2020-07-05 17:46:35 +0100

    quick

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - argv display
+ * @argc: var
+ * @argv: str
+ * Return: 0
+ */
+int main(int argc, __attribute__((unused))char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
