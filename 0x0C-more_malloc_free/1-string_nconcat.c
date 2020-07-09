f22a48b13139dca0e5ce4bf6f08c4a8f79bd0d4d
JSalmen13 <jarraya.salmen13@gmail.com>
2020-07-09 19:06:04 +0100

    more fixes and changes

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,64 +1,48 @@
 #include "holberton.h"
-char *a1(char *s1, char *s2, unsigned int i, unsigned int j,
-unsigned int n, unsigned int k, unsigned int len)
-{
-	char *dest;
-
-	while (s1[i])
-		i++;
-	while (s2[j])
-		j++;
-	if (j > n)
-		k = n;
-	else
-		k = j;
-	dest = malloc(i + k + 1);
-	if (dest != NULL)
-	{
-		for (len = 0; s1[len]; len++)
-		dest[len] = s1[len];
-		for (; len < i + k; len++)
-		dest[len] = s2[len - i];
-		dest[len] = '\0';
-		return (dest);
-	}
-	else
-		return (NULL);
-}
 /**
- * string_nconcat - string contact
+ * string_nconcat - concatenate 2 strings limited by n
  * @s1: str
  * @s2: str
- * @n: int
- * Return: success
+ * @n: number of chars to concat
+ * Return: dest
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i = 0, j = 0, k = 0, len = 0;
 	char *dest;
+	unsigned int i = 0, j = 0, k = 0, r = 0;
 
 	if (s1 != NULL)
 	{
-		return (a1(s1, s2, i, j, n, k, len));
+		while (s1[i])
+			i++;
 	}
-	else
+
+	if (s2 != NULL)
 	{
 		while (s2[j])
 			j++;
-		if (j > n)
-			k = n;
-		else
-			k = j;
-		dest = malloc(k + 1);
-		if (dest != NULL)
+	}
+	r = j - n;
+	if (n >= j)
+	{
+		r = 0;
+	}
+	dest = malloc(((i + j + 1) - r) * sizeof(char));
+	if (dest == NULL)
+		return (NULL);
+	while (k < ((i + j) - r))
+	{
+		if (k < i)
 		{
-			for (len = 0; len < k; len++)
-			dest[len] = s2[len];
-			dest[len] = '\0';
-			return (dest);
+			dest[k] = s1[k];
+			k++;
 		}
 		else
-			return (NULL);
+		{
+			dest[k] = s2[k - i];
+			k++;
+		}
 	}
-	return (NULL);
+	dest[k] = '\0';
+	return (dest);
 }
