f32b62a05a2fda01b20f1fca8b1e18f0700164e9
JSalmen13 <jarraya.salmen13@gmail.com>
2020-07-09 18:06:03 +0100

    40lines my ass

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,6 +1,30 @@
 #include "holberton.h"
-#include <stdio.h>
-#include <stdlib.h>
+char *a1(char *s1, char *s2, unsigned int i, unsigned int j,
+unsigned int n, unsigned int k, unsigned int len)
+{
+	char *dest;
+
+	while (s1[i])
+		i++;
+	while (s2[j])
+		j++;
+	if (j > n)
+		k = n;
+	else
+		k = j;
+	dest = malloc(i + k + 1);
+	if (dest != NULL)
+	{
+		for (len = 0; s1[len]; len++)
+		dest[len] = s1[len];
+		for (; len < i + k; len++)
+		dest[len] = s2[len - i];
+		dest[len] = '\0';
+		return (dest);
+	}
+	else
+		return (NULL);
+}
 /**
  * string_nconcat - string contact
  * @s1: str
@@ -10,34 +34,17 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i = 0, j = 0, k, len;
+	unsigned int i = 0, j = 0, k = 0, len = 0;
 	char *dest;
 
 	if (s1 != NULL)
 	{
-		while (s1[i])
-			i++;
-		while (s2[j])
-			j++;
-		if (j > n)
-			k = n;
-		else
-			k = j;
-		dest = malloc(i + k + 1);
-		if (dest != NULL)
-		{
-			for (len = 0; s1[len]; len++)
-			dest[len] = s1[len];
-			for (; len < i + k; len++)
-			dest[len] = s2[len - i];
-			dest[len] = '\0';
-			return (dest);
-		}
-		else
-			return (NULL);
+		return (a1(s1, s2, i, j, n, k, len));
 	}
 	else
 	{
+		while (s2[j])
+			j++;
 		if (j > n)
 			k = n;
 		else
@@ -45,9 +52,13 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		dest = malloc(k + 1);
 		if (dest != NULL)
 		{
-			for (len = 0; len ; len++)
+			for (len = 0; len < k; len++)
 			dest[len] = s2[len];
+			dest[len] = '\0';
+			return (dest);
 		}
-		return (dest);
+		else
+			return (NULL);
 	}
+	return (NULL);
 }
