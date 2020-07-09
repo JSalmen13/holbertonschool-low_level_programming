c0161851f640f5f6810587e09772422376e3b707
JSalmen13 <jarraya.salmen13@gmail.com>
2020-07-09 20:03:21 +0100

    brainstoorm

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ * array_range - array of intergers
+ * @min: var
+ * @max: var
+ * Return: an array of ints
+ */
+int *array_range(int min, int max)
+{
+	int *num;
+	int i = 0, j;
+
+	if (min > max)
+		return (NULL);
+	num = malloc(((max - min) + 1) * sizeof(int));
+	if (num == NULL)
+		return (NULL);
+	j = max;
+	while (i <= (max - min))
+	{
+		num[i] = j;
+		j++;
+		i++;
+	}
+	return (num);
+}
