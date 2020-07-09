871927f070d8de756a0024ca2072a6e8e5c1f218
JSalmen13 <jarraya.salmen13@gmail.com>
2020-07-09 13:03:28 +0100

    fixes

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,1 +1,20 @@
 #include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * malloc_checked - check if malloc fail
+ * @b: int
+ * Return: always success on every project
+ */
+void *malloc_checked(unsigned int b)
+{
+	char *x;
+
+	x = malloc(b);
+	if (x == NULL)
+	{
+	exit(98);
+	}
+	else
+	return (x);
+}
