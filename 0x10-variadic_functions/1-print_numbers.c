8ffce1008fcf17483a290e5b443b1069db89174f
Jsalmen13 <Jarraya.salmen13@gmail.com>
2020-07-19 16:32:26 +0100

    faith in humanity restored

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+/**
+ * 
+ * 
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list k;
+	unsigned int i = 0;
+
+	va_start(k, n);
+	if (separator == NULL)
+	{
+		return;
+	}	
+	while (i < n)
+	{
+		printf("%d", va_arg(k, int));
+		if(i != n - 1)
+		printf("%s", separator);
+		i++;
+	}
+	va_end(k);
+	printf("\n");
+}
