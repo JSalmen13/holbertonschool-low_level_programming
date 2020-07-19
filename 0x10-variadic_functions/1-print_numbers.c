d54b045b82ab92916e7255e663fc5b73a6336f2d
Jsalmen13 <Jarraya.salmen13@gmail.com>
2020-07-19 17:54:27 +0100

    faith

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -10,14 +10,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i = 0;
 
 	va_start(k, n);
-	if (separator == NULL || n == 0)
-	{
-		return;
-	}
 	while (i < n)
 	{
 		printf("%d", va_arg(k, int));
-		if (i != n - 1)
+		if (i != n - 1 && separator)
 		printf("%s", separator);
 		i++;
 	}
