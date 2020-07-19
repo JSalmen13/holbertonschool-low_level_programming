5fbfb7f7f2e06581dc8f497991885e8d7e906673
Jsalmen13 <Jarraya.salmen13@gmail.com>
2020-07-19 18:19:32 +0100

    fix

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+/**
+ * print_strings - prints string with sep
+ * @separator: sep
+ * @n: int
+ * Return: 0
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+    va_list k;
+	unsigned int i = 0;
+    char *p;
+
+	va_start(k, n);
+	while (i < n)
+	{
+        p = (va_arg(k, char *));
+        if (p == NULL)
+            printf("(nil)");
+        else
+		printf("%s", p);
+		if (i != n - 1 && separator)
+		printf("%s", separator);
+		i++;
+	}
+	va_end(k);
+	printf("\n");
+}
