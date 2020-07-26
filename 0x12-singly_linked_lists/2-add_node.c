fb7404801aa85094d0709edaa2ea019a82b2ded5
Jsalmen13 <Jarraya.salmen13@gmail.com>
2020-07-26 16:02:39 +0100

    add

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+/**
+ * 
+ * 
+ * 
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	unsigned int k;
+	list_t *Node;
+	char *s;
+
+	Node = malloc(sizeof(size_t));
+	if (Node == NULL)
+		return (NULL);
+	if (str == NULL)
+		return (NULL);
+
+	k = 0;
+	while (str[k] != '\0')
+		k++;
+	s = strdup(str);
+	Node->str = s;
+	Node->len = k;
+	Node->next = *head;
+	*head = Node;
+	return (Node);
+}
