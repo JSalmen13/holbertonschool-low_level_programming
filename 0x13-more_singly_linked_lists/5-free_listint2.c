cccd1b8ecabb4750d5fe0dc1920ee3e981da80b7
Jsalmen13 <Jarraya.salmen13@gmail.com>
2020-07-29 20:57:39 +0100

    more tasks

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * free_listint2 - sets head to NULL
+ * @head: element 
+ * Return: NULL
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	temp = NULL;
+	if (head == NULL)
+		return;
+	while (*head)
+	{
+		temp = *head;
+		*head = (*head)->next;
+		free(temp);
+	}
+	*head = NULL;
+}
